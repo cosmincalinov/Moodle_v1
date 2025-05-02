ae65e1560d492e489d9c1f0376a40f211abe4c59
cosmincalinov <cosmin.calinov@yahoo.com>
2025-05-02 17:21:17 +0300

    Add project files.

---
+++ Moodle_v1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moodle_v1.Areas.Identity.Data;
+using Moodle_v1.Models;
+
+namespace Moodle_v1.Controllers;
+[Authorize]
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public HomeController(ILogger<HomeController> logger,UserManager<ApplicationUser> userManager)
+    {
+        _logger = logger;
+        this._userManager = userManager;
+    }
+
+    public IActionResult Index()
+    {
+        ViewData["UserID"] = _userManager.GetUserId(this.User);
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Moodle_v1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moodle_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
